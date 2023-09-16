0afeb54b8e2b285dc4f1d360e84c5cd5dedf5c54
Dragon-1645 <145196893+Dragon-1645@users.noreply.github.com>
2023-09-16 17:57:54 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,17 @@
+name = input(str("whats your name? "))
+q = input(str("great! do you want to play a game? "))
+if q =="yes":
+    print("lets start")
+elif q=="Yes":
+    print("lets start")
+else:
+    print("ok fine bye")
+q1 = input(str("do you have a dog? "))
+if q1=="yes":
+    dog = input(str("whats its name? "))
+    print(dog)
+if q1=="Yes":
+    dog = input(str("whats its name? "))
+    print(dog)
+else:
+    print("fine")
